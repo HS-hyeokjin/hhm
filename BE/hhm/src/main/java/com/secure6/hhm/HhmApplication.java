1f992afb60c012646ef2b01cf084483634955a60
HS-hyeokjin <youngurwls@gmail.com>
2022-11-02 18:07:48 +0900

    init

---
+++ BE/hhm/src/main/java/com/secure6/hhm/HhmApplication.java
@@ -0,0 +1,13 @@
+package com.secure6.hhm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhmApplication.class, args);
+	}
+
+}
---
+++ BE/hhm/src/test/java/com/secure6/hhm/HhmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secure6.hhm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
